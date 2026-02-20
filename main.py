137f77fb8117828a97549fd0a56ad5b6c5ad6d1d
Stefan Jenss <stefanjenss@gmail.com>
2026-02-19 23:48:44 -0500

    Initial commit: You can currently interface with the AI through the CLI by running the main.py function in the terminal along with a string of your answer (i.e., 'What is the meaning of life'?) | Set up calculator app test codebase to allow the CLI agent to index and response to bug fixes for

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+from google.genai import types
+
+def main():
+    print("Hello from python-ai-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("The API KEY was not found")
+
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # --- Regerate and return the model's response ---
+
+    agent_response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = messages
+    )
+
+    if agent_response is None or agent_response.usage_metadata is None:
+        raise RuntimeError("Either the agent_response or '.usage_metadata' is 'None'")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {agent_response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {agent_response.usage_metadata.candidates_token_count}")
+
+    print(agent_response.text)
+
+if __name__ == "__main__":
+    main()
+
